90d70529f2b710866ecfb3ac505a77fec43fef89
Vasyl <44724842+morf-1-3@users.noreply.github.com>
2025-03-09 15:49:17 +0200

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,7 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # client = AsyncIOMotorClient("mongodb://localhost:27017/")
-client = AsyncIOMotorClient("mongodb://mongo:rhwkDKoIuBhvIPiQPopbzTRBuxzmtIwM@mainline.proxy.rlwy.net:45623")
+# client = AsyncIOMotorClient("mongodb://mongo:rhwkDKoIuBhvIPiQPopbzTRBuxzmtIwM@mainline.proxy.rlwy.net:45623")
+client = AsyncIOMotorClient("mongodb://mongo:XxMXchiutmxzJAUBQCEGHkrOsjlEtwjp@metro.proxy.rlwy.net:31526")
+
 
 # створили взяли бд
 db = client["my_database"]
